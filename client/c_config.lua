917d43571e34ea348fbf051b564027f48b452f80
RSG -Realstonergamer <xanten11@gmail.com>
2023-07-05 15:08:34 -0400

    add missing guy ped config for females
    add missing guy ped config for females

--- client/c_config.lua
+++ client/c_config.lua
@@ -6,6 +6,7 @@ Config.BathingZones = {
 		rag = vector4(2629.4, -1223.33, 58.57, -92.66),
 		consumer = vector3(2632.6, -1223.79, 59.59),
 		lady = `CS_BATHINGLADIES_01`,
+		guy = `CS_LeviSimon`,
 		door = 779421929
 	},
 	["Valentine"] = {
@@ -13,6 +14,7 @@ Config.BathingZones = {
 		rag = vector4(-317.37, 761.8, 116.44, 10.365),
 		consumer = vector3(-320.56, 762.41, 117.44),
 		lady = `CS_BATHINGLADIES_01`,
+		guy = `CS_LeviSimon`,
 		door = 142240370
 	},
 	["Annesburg"] = {
@@ -20,6 +22,7 @@ Config.BathingZones = {
 		rag = vector4(2952.65, 1334.7, 43.44, -291.27),
 		consumer = vector3(2950.42, 1332.15, 44.44),
 		lady = `CS_BATHINGLADIES_01`,
+		guy = `CS_LeviSimon`,
 		door = -201071322
 	},
 	["Strawberry"] = {
@@ -27,6 +30,7 @@ Config.BathingZones = {
 		rag = vector4(-1812.83, -373.23, 165.5, 1.206),
 		consumer = vector3(-1816.45, -372.44, 166.50),
 		lady = `CS_BATHINGLADIES_01`,
+		guy = `CS_LeviSimon`,
 		door = 1256786197
 	},
 	["Blackwater"] = {
@@ -34,6 +38,7 @@ Config.BathingZones = {
 		rag = vector4(-823.86, -1318.84, 42.68, -0.459),
 		consumer = vector3(-822.82, -1315.72, 43.58),
 		lady = `CS_BATHINGLADIES_01`,
+		guy = `CS_LeviSimon`,
 		door = 1523300673
 	},
 	["Vanhorn"] = {
@@ -41,6 +46,7 @@ Config.BathingZones = {
 		rag = vector4(2987.62, 573.21, 46.86, 83.841),
 		consumer = vector3(2986.31, 568.27, 47.85),
 		lady = `CS_BATHINGLADIES_01`,
+		guy = `CS_LeviSimon`,
 		door = 1102743282
 	},
 	["Rhodes"] = {
@@ -48,6 +54,7 @@ Config.BathingZones = {
 		rag = vector4(1336.85, -1378.04, 83.2897, 166.469),
 		consumer = vector3(1340.11, -1379.6, 84.28),
 		lady = `CS_BATHINGLADIES_01`,
+		guy = `CS_LeviSimon`,
 		door = -1847993131
 	},
 	--[[["Tumbleweed"] = {
@@ -55,6 +62,7 @@ Config.BathingZones = {
 		rag = vector4(-5513.76, -2972.3, -1.78, 15.0),
 		consumer = vector3(-5517.83, -2973.23, -0.78),
 		lady = `CS_BATHINGLADIES_01`,
+		guy = `CS_LeviSimon`,
 		door = 1682160693
 	}]]
 }
